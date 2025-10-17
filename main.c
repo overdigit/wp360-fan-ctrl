809f9ee9c0b5edf87792965de85fe30a3c1514eb
Nicola Orlando <niki.or97@yahoo.it>
2025-10-17 16:33:22 +0200

    Update to libgpiod3

--- main.c
+++ main.c
@@ -11,10 +11,9 @@
 #define	CONSUMER "wp360-fan-governor"
 #endif
 
+#define FAN_PERIOD  3600
 #if DEBUG
 #define FAN_PERIOD  60
-#else
-#define FAN_PERIOD  3600
 #endif
 
 #define IOCTL_MBOX_PROPERTY _IOWR(100, 0, char *)
@@ -55,9 +54,12 @@ float vcgencmd_measure_temp(int *err)
 
 int main(void)
 {
-  char *chipname = "gpiochip0";
+  char *chipname = "/dev/gpiochip0";
   struct gpiod_chip *chip;
-  struct gpiod_line *line[2];
+  struct gpiod_line_request *line;
+  struct gpiod_line_config *config;
+  struct gpiod_line_settings *settings;
+  struct gpiod_request_config *req_cfg;
   int pin_fan[2] = {26, 27};
   int ret;
   unsigned char return_code = 0;
@@ -78,7 +80,7 @@ int main(void)
   fprintf(log,"Time,Temperature,fan_low,fan_high,fan0,fan1\n");
 #endif
 
-  chip = gpiod_chip_open_by_name(chipname);
+  chip = gpiod_chip_open(chipname);
   if (!chip)
   {
     perror("Open chip failed\n");
@@ -86,21 +88,56 @@ int main(void)
     goto end;
   }
 
-  line[0] = gpiod_chip_get_line(chip, pin_fan[0]);
-  line[1] = gpiod_chip_get_line(chip, pin_fan[1]);
-  if (!(line[0] && line[1]))
+  settings = gpiod_line_settings_new();
+  if (settings == NULL)
   {
-    perror("Get line failed\n");
+    perror("Could not initialize line settings\n");
     return_code = 254;
     goto close_chip;
   }
 
-  ret = gpiod_line_request_output(line[0], CONSUMER, 0) || gpiod_line_request_output(line[1], CONSUMER, 0);
-  if (ret < 0)
+  if (
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT) ||
+    gpiod_line_settings_set_drive(settings, GPIOD_LINE_DRIVE_PUSH_PULL) ||
+    gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_INACTIVE)
+  )
   {
-    perror("Request line as output failed\n");
+    perror("Could not set line settings\n");
     return_code = 253;
-    goto release_line;
+    goto release_settings;
+  }
+
+  config = gpiod_line_config_new();
+  if (config == NULL)
+  {
+    perror("Could not initialize line configuration\n");
+    return_code = 252;
+    goto release_settings;
+  }
+  if (
+    gpiod_line_config_add_line_settings(config, pin_fan, 2, settings)
+  )
+  {
+    perror("Could not set line configuration\n");
+    return_code = 251;
+    goto release_config;
+  }
+
+  req_cfg = gpiod_request_config_new();
+  if (req_cfg == NULL)
+  {
+    perror("Could not initialize request configuration\n");
+    return_code = 250;
+    goto release_config;
+  }
+  gpiod_request_config_set_consumer(req_cfg, CONSUMER);
+
+  line = gpiod_chip_request_lines(chip, req_cfg, config);
+  if (line == NULL)
+  {
+    perror("Could not request line\n");
+    return_code = 248;
+    goto release_req_cfg;
   }
 
   while (true)
@@ -115,8 +152,13 @@ int main(void)
     fan_low  = temp_deg > 65.0 || (fan_low  && temp_deg > 60.0);
     fan_high = temp_deg > 87.0 || (fan_high && temp_deg > 83.0);
 
-    gpiod_line_set_value(line[fan_time <= FAN_PERIOD/2],  fan_low);
-#if DEBUG
+    if (gpiod_line_request_set_value(line, pin_fan[fan_time <= FAN_PERIOD/2], fan_low ? GPIOD_LINE_VALUE_ACTIVE : GPIOD_LINE_VALUE_INACTIVE))
+    {
+      perror("Could not set line value\n");
+      return_code = 247;
+      goto release_line;
+    }
+#if DEBUGTODOFIXTHISPART
     unix_time = time(NULL);
     strftime(date_fmt, 256, "%Y-%m-%d %H:%M:%S", gmtime(&unix_time));
     fprintf(stderr, "%s, Temp: %3.1f, fan_low: %d, fan_high: %d, fan0: %d, fan1: %d\n", date_fmt, temp_deg, fan_low, fan_high, gpiod_line_get_value(line[0]), gpiod_line_get_value(line[1]));
@@ -124,15 +166,25 @@ int main(void)
     fflush(log);
 #endif
     sleep(1);
-    gpiod_line_set_value(line[fan_time > FAN_PERIOD/2], fan_high);
+    if (gpiod_line_request_set_value(line, pin_fan[fan_time > FAN_PERIOD/2], fan_high ? GPIOD_LINE_VALUE_ACTIVE : GPIOD_LINE_VALUE_INACTIVE))
+    {
+      perror("Could not set line value\n");
+      return_code = 247;
+      goto release_line;
+    }
     if (fan_low && !fan_high) {if(++fan_time > FAN_PERIOD-1) fan_time = 0;}
   }
 #if DEBUG
   fclose(log);
 #endif
 release_line:
-  gpiod_line_release(line[0]);
-  gpiod_line_release(line[1]);
+  gpiod_line_request_release(line);
+release_req_cfg:
+  gpiod_request_config_free(req_cfg);
+release_config:
+  gpiod_line_config_free(config);
+release_settings:
+  gpiod_line_settings_free(settings);
 close_chip:
   gpiod_chip_close(chip);
 end:
